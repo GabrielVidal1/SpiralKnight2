0a1a178c413d1428fd5cd303985a7778639d7012
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-04-28 22:30:50 +0200

    add(Enemy.cs): Add enemy attack when close to the target player

--- SpiralKnight/Assets/Scripts/Multiplayer/Server/Entities/Enemy.cs
+++ SpiralKnight/Assets/Scripts/Multiplayer/Server/Entities/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using Multiplayer.Client.Entities;
 using UnityEngine;
@@ -9,13 +10,20 @@ namespace Multiplayer.Server.Entities
     [RequireComponent(typeof(NavMeshAgent))]
     public class Enemy : Entity
     {
+        private const float Range = 2f;
+
         private NavMeshAgent _agent;
         private Transform _target;
+
+        private bool _attacking;
+
+        private float _speed;
         
         public override void Initialize(float _maxHealth)
         {
             base.Initialize(_maxHealth);
             _agent = GetComponent<NavMeshAgent>();
+            _speed = _agent.speed;
         }
 
         public override void Die()
@@ -26,9 +34,31 @@ namespace Multiplayer.Server.Entities
         protected override void EachTick()
         {
             if (_target == null) return;
+
+            if (_attacking) return;
             
-            _agent.SetDestination(_target.position);
-            direction = transform.forward;
+            if ((_target.position - transform.position).sqrMagnitude < Range * Range)
+            {
+                StartCoroutine(Attack());
+            }
+            else
+            {
+                _agent.SetDestination(_target.position);
+                direction = transform.forward;
+            }
+        }
+
+        IEnumerator Attack()
+        {
+            _attacking = true;
+            _agent.speed = 0;
+            
+            
+            yield return new WaitForSeconds(0.5f);
+            base.Attack(transform.forward);
+            yield return new WaitForSeconds(0.5f);
+            _agent.speed = _speed;
+            _attacking = false;
         }
 
         public void SetTarget(Entity _player)
